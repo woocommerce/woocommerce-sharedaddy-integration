0ae4b22e269c3a35651c928bbbbfc1d194d22eee
Coen Jacobs <coenjacobs@gmail.com>
2013-11-22 10:52:36 +0100

    Require WooCommerce 2.1 for this to work

--- wc-sharedaddy-integration.php
+++ wc-sharedaddy-integration.php
@@ -10,9 +10,13 @@ Version: 1.0
 
 // Add the integration to WooCommerce
 function add_sharedaddy_integration( $integrations ) {
-    if ( class_exists('jetpack') ) {
-        include_once( 'includes/class-wc-sharedaddy-integration.php' );
-        $integrations[] = 'WC_ShareDaddy';
+    global $woocommerce;
+
+    if ( is_object( $woocommerce ) && version_compare( $woocommerce->version, '2.1', '>=' ) ) {
+        if ( class_exists('jetpack') ) {
+            include_once( 'includes/class-wc-sharedaddy-integration.php' );
+            $integrations[] = 'WC_ShareDaddy';
+        }
     }
 
     return $integrations;
