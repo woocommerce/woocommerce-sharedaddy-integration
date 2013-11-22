4e1c7ee4e77ff69d044d0644374113dc2bad957e
Coen Jacobs <coenjacobs@gmail.com>
2013-11-22 10:50:49 +0100

    Split up plugin loader and class file

--- wc-sharedaddy-integration.php
+++ wc-sharedaddy-integration.php
@@ -10,80 +10,12 @@ Version: 0.1
 
 // Add the integration to WooCommerce
 function add_sharedaddy_integration( $integrations ) {
-    if ( class_exists('jetpack') )
+    if ( class_exists('jetpack') ) {
+        include_once( 'includes/class-wc-sharedaddy-integration.php' );
         $integrations[] = 'WC_ShareDaddy';
+    }
+    
     return $integrations;
 }
 
-add_filter('woocommerce_integrations', 'add_sharedaddy_integration' );
-
-/**
- * ShareDaddy Integration
- *
- * Enables ShareDaddy integration.
- *
- * @class 		WC_ShareDaddy
- * @extends		WC_Integration
- */
-class WC_ShareDaddy extends WC_Integration {
-
-	/**
-	 * Init and hook in the integration.
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function __construct() {
-        $this->id					= 'sharedaddy';
-        $this->method_title     	= __( 'ShareDaddy', 'woocommerce' );
-        $this->method_description	= __( 'ShareDaddy is a sharing plugin bundled with JetPack.', 'woocommerce' );
-
-		// Load the settings.
-		$this->init_form_fields();
-		$this->init_settings();
-
-		// Actions
-		add_action( 'woocommerce_update_options_integration_sharedaddy', array( $this, 'process_admin_options' ) );
-
-		// Share widget
-		add_action( 'woocommerce_share', array( $this, 'sharedaddy_code' ) );
-    }
-
-
-    /**
-     * Initialise Settings Form Fields
-     *
-     * @access public
-     * @return void
-     */
-    function init_form_fields() {
-
-    	$this->form_fields = array(
-			'enabled' => array(
-				'title' 		=> __( 'Output ShareDaddy button?', 'woocommerce' ),
-				'description' 	=> __( 'Enable this option to show the ShareDaddy button on the product page.', 'woocommerce' ),
-				'type' 			=> 'checkbox',
-				'default' 		=> get_option('woocommerce_sharedaddy') ? get_option('woocommerce_sharedaddy') : 'no'
-			)
-		);
-
-    }
-
-
-    /**
-     * Output share code.
-     *
-     * @access public
-     * @return void
-     */
-    function sharedaddy_code() {
-    	global $post;
-
-    	if ( $this->enabled == 'yes' && function_exists('sharing_display') ) {
-
-    		?><div class="social"><?php echo sharing_display(); ?></div><?php
-
-    	}
-    }
-
-}
+add_filter( 'woocommerce_integrations', 'add_sharedaddy_integration' );
